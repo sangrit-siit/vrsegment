afd245d34af517182416ed6568e3f0de645ab919
sangrit <Kittikom.San@dome.tu.ac.th>
2018-02-13 19:43:47 +0700

    Combine work

---
+++ vrsegment/segmentation.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+
+import os.path
+import sys
+import json
+
+dictionary_file_path = "../src/corpus/dictionary.json"
+dictionary_file  = open(dictionary_file_path,'r')
+dictionary = json.load(dictionary_file)
+
+test_set_file = "../src/test/test_set.txt"
+
+key_set_file = "../src/key/key_set.txt"
+
+def check(word):
+    global dictionary
+    if word in dictionary:
+        return True
+    else:
+        return False
+#end def
+
+def forwardCut(sentence):
+    list_position = []
+    list_answer = []
+    start = 0
+    end = 0
+    i = 0
+    while i < len(sentence):
+        answer = list()
+        position = list()
+        end = start
+        for j in range(i, len(sentence) + 1):
+            word = sentence[i:j]
+            if check(word):
+                #print(word)
+                last_word = ""
+                if len(answer) != 0:
+                    last_word = answer.pop()
+                    position = position.pop()
+                if i != start:
+                    try:
+                        list_answer.pop()
+                        list_position.pop()
+                    except IndexError:
+                        pass
+                    list_answer.append(sentence[start:i])
+                    list_position.append(i)
+                answer.append(word)
+                position.append([i])
+                end = j
+        if end == start:
+            i = i+1
+        else:
+            start = end
+            i = start
+            list_answer.append(answer[-1])
+            list_position.append(position[-1])
+            #end if
+        #end for
+    #end while
+    if i != start:
+        list_answer.append(sentence[start:])
+    return list_answer
+#end def
+
+def backwardCut(sentence):
+    #reverse the sentence
+
+    # search from back of the string (front of reversed)
+        # increment the lenngth of word-to-be-cut
+        # if found in the dictionary keep the word as maxlength
+        # else pass
+    # if maxlength isset
+        # return maxlength
+    # else
+        # word not found in dictionary skip one char and retry
+
+    # return [left,word,right]
+
+    [inleft, inskipword, inright] = sentence
+    max_word = ''
+    reversed_sentence = inleft[::-1]
+    for length in range(1,len(reversed_sentence)+1):
+        word = reversed_sentence[:length][::-1]
+        if(word in dictionary):
+            # search dictionary
+            max_word = word
+            ## print('found = '+max_word)
+        else:
+            pass
+
+    if (max_word != ''):
+        ## found in dictionary
+        left = inleft[:-len(max_word)]
+        skipword = ''
+        right = max_word
+        #print("Left = "+left+" word = "+word)
+        return [backwardCut([left,'','']),skipword,right]
+    else:
+        ## not found in dictionary or done
+        try:
+            left = inleft[:-len(max_word)-1]
+            skipword = inleft[-1]
+            right = ''
+            #print("Left = "+left+" word = "+word)
+            return [backwardCut([left,'','']),skipword,right]
+        except IndexError:
+            print("DONE")
+
+def recursivePrint(sentencelist,string):
+    [left, skipword, right] = sentencelist
+    if(type(left) == type(['1'])):
+        if(skipword == ''):
+            if(string[0] == '|'):
+                recursivePrint(left,"|"+right+string)
+            else:
+                recursivePrint(left,right+"|"+string)
+        else:
+            recursivePrint(left,skipword+string)
+    else:
+        ## print back
+        print(right+"|"+string)
+
+def test():
+    print("Testing VRSegment")
+
+    try:
+        test_file = open(test_set_file, 'r',encoding="UTF-8")
+        key_file  = open(key_set_file, 'r',encoding="UTF-8")
+        num_lines = len(open(test_set_file).readlines())
+        for line in test_file:
+            sentence = line.strip("\n")
+            key = key_file.readline().strip("\n")
+            forwardCutted = forwardCut(sentence)
+            backwardCutted = backwardCut([sentence,'',''])
+            print(">> input:\t"+sentence)
+            print(">> key:\t\t"+key)
+            print(">> output 1:\t"+"|".join(forwardCutted))
+            print(">> output 2:\t",end="")
+            recursivePrint(backwardCutted,"|")
+            print()
+        #end for
+        test_file.close()
+    except FileNotFoundError:
+        pass
+#end def
